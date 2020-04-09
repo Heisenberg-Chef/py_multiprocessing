c998296a405d2dde1c18a46d0dc939882dcb2553
Heisenberg <825958926@qq.com>
2020-04-09 22:11:48 +0800

    master

---
+++ 创建简单多进程/main.py
@@ -0,0 +1,13 @@
+import multiprocessing
+import time
+
+def foo(i):
+    print('say hi',i)
+    while 1:
+        pass
+
+if __name__ == '__main__':
+    for i in range(10):
+        p = multiprocessing.Process(target=foo,args=(i,))
+        p.start()
+        print(p.pid)
