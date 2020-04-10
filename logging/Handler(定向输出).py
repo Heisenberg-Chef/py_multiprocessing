e3a1b8ba4aa51060b1bdf0b71fa2194e306b9b22
Ray <qilei@Heisenberg.local>
2020-04-10 11:13:50 +0800

    master

---
+++ logging/Handler(定向输出).py
@@ -0,0 +1,15 @@
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+handler = logging.FileHandler("log.txt")
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+logger.info("Start print log")
+logger.debug("Do something")
+logger.warning("Something maybe fail.")
+logger.info("Finish")
+
+#  初始化一个logger对象，然后对其进行handler设置定义 格式、输出等级、输出的目标
