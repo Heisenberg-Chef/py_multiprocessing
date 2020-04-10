750755f3bfee2ca0b8b840152fb7c9bae6e17a38
Ray <825958926@qq.com>
2020-04-10 20:46:06 +0800

    修改

---
+++ 自定义进程/main.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8 -*-
+import multiprocessing
+import time
+
+def foo(i):
+    print('say hi',i)
+    
+for i in range(10):
+    p = multiprocessing.Process(target=foo,args=(i,))
+    p.start()
